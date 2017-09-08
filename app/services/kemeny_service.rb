dc59ac657072b919252b1bb78c3c54ec07767caa
s103619 <s103619@student.dtu.dk>
2017-09-08 14:00:22 +0200

    Kemeny-Young method finished

--- app/services/kemeny_service.rb
+++ app/services/kemeny_service.rb
@@ -6,6 +6,7 @@ class KemenyService
     @input = input
   end
 
+  """
   def compareTwo(x, y)
     xGtY = 0
     xEqY = 0
@@ -30,12 +31,51 @@ class KemenyService
         end
       end
     end
-    print "Results: #{x} #{y} #{[xGtY, xEqY, xLtY]} \n"
     [xGtY, xEqY, xLtY]
   end
+  """
 
-  def pairCombination(movies)
-    movies.combination(2).to_a
+  def getScore(x,y )
+    score = 0
+    @input.each do |_,profile|
+      yFound = false
+      profile.each do |b|
+        if b.include?(x)
+          if !yFound
+            score += 1
+          end
+        elsif b.include?(y)
+          yFound = true
+        end
+      end
+    end
+    score
+  end
+
+  def pairwiseJunk(pairs)
+    scores = Hash.new
+    pairs.each do |ranking|
+      score = 0
+      ranking.combination(2).to_a.each do |x, y|
+        score += getScore(x, y)
+      end
+      scores[ranking] = score
+    end
+    scores
+  end
+
+  def choicePairs(choices)
+    choices.combination(2).to_a
+  end
+
+  def preferenceOrderings(choices)
+    choices.permutation(choices.length).to_a
+  end
+
+  def getWinner(movies)
+    prefs = preferenceOrderings(movies)
+    rankingScores = pairwiseJunk(prefs)
+    rankingScores.max_by{|k,v| v}
   end
 
 end
--- spec/services/kemeny_service_spec.rb
+++ spec/services/kemeny_service_spec.rb
@@ -5,36 +5,41 @@ require_relative './../../app/services/parser_service.rb'
 
 RSpec.describe KemenyService do
   before :each do
-    lexer = LexerService.new(File.readlines(File.expand_path('spec/files/test_strong_preferences.txt')))
+    #lexer = LexerService.new(File.readlines(File.expand_path('spec/files/test_strong_preferences.txt')))
+    lexer = LexerService.new(File.readlines(File.expand_path('spec/files/test_weak_preferences.txt')))
     @parser = ParserService.new(lexer.tokens)
     @parser.parse_preference(lexer.preferences)
     @movies = @parser.parse[:movies]
     @kemeny = KemenyService.new(@parser.parse[:preferences])
   end
 
-  describe '#compareTwo' do
-    it 'initializes properly' do
-      warcraft = @movies[0]
-      superman = @movies[2]
-      expect(@kemeny.compareTwo(warcraft, superman)).to eq([2,0,1])
+  describe '#pairCombinations' do
+    it 'gets correct pair combinations from parser input' do
+      expect(@kemeny.preferenceOrderings(@movies)).to eq([["Warcraft", "Batman", "Superman"], ["Warcraft", "Superman", "Batman"], ["Batman", "Warcraft", "Superman"], ["Batman", "Superman", "Warcraft"], ["Superman", "Warcraft", "Batman"], ["Superman", "Batman", "Warcraft"]])
+      expect(@kemeny.choicePairs(@movies)).to eq([["Warcraft", "Batman"], ["Warcraft", "Superman"], ["Batman", "Superman"]])
     end
   end
 
-  describe '#pairCombinations' do
-    it 'gets correct pair combinations from parser input' do
-      expect(@kemeny.pairCombination(@movies)).to eq([["Warcraft", "Batman"], ["Warcraft", "Superman"], ["Batman", "Superman"]])
+  describe '#preferenceRankings' do
+    it 'gets correct preference rankings from parser input' do
+      expect(@kemeny.preferenceOrderings(@movies)).to eq([["Warcraft", "Batman", "Superman"], ["Warcraft", "Superman", "Batman"], ["Batman", "Warcraft", "Superman"], ["Batman", "Superman", "Warcraft"], ["Superman", "Warcraft", "Batman"], ["Superman", "Batman", "Warcraft"]])
     end
   end
 
   describe '#pairCombinations' do
     it 'all pairs' do
-      @kemeny.pairCombination(@movies).each do |x, y|
-        @kemeny.compareTwo(x, y)
-      end
+      warcraft = @movies[0]
+      superman = @movies[2]
+      print "Score #{warcraft} > #{superman}: #{@kemeny.getScore(warcraft, superman)}\n"
+      print "Slices: #{@kemeny.preferenceOrderings(@movies)[0]}\n"
     end
   end
 
-
+  describe 'find winner' do
+    it 'winner?' do
+      print "Winner: #{@kemeny.getWinner(@movies)}\n"
+    end
+  end
 
   """
   describe '#voters' do
