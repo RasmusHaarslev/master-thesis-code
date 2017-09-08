77196802cbec28e3780ac1816b667d3447c2c373
s103619 <s103619@student.dtu.dk>
2017-09-08 14:07:14 +0200

    Nothing changed, but Git wanted a commit regardless before merging into master.

--- app/services/kemeny_service.rb
+++ app/services/kemeny_service.rb
@@ -6,35 +6,6 @@ class KemenyService
     @input = input
   end
 
-  """
-  def compareTwo(x, y)
-    xGtY = 0
-    xEqY = 0
-    xLtY = 0
-
-    @input.each do |_, profile|
-      xFound = false
-      yFound = false
-      profile.each do |b|
-        if b.include?(x) && b.include?(y)
-          xEqY += 1
-        elsif b.include?(x)
-          if !yFound
-            xGtY += 1
-          end
-          xFound = true
-        elsif b.include?(y)
-          if !xFound
-            xLtY += 1
-          end
-          yFound = true
-        end
-      end
-    end
-    [xGtY, xEqY, xLtY]
-  end
-  """
-
   def getScore(x,y )
     score = 0
     @input.each do |_,profile|
