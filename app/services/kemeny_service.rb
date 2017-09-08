a4b0307b9984a1715545798e65b01f574ca32acf
s103619 <s103619@student.dtu.dk>
2017-09-08 11:42:08 +0200

    Works with lexer/parser and can create tally scores. Still missing rank scores.

--- app/services/kemeny_service.rb
+++ app/services/kemeny_service.rb
@@ -1,37 +1,41 @@
 class KemenyService
 
-    attr_accessor :input
+  attr_accessor :input
 
-    def initialize(input)
-      @input = input
-    end
+  def initialize(input)
+    @input = input
+  end
 
-    def compareTwo(x, y)
-        xGtY = 0
-        xEqY = 0
-        xLtY = 0
+  def compareTwo(x, y)
+    xGtY = 0
+    xEqY = 0
+    xLtY = 0
 
-        for a in input
-            xFound = false
-            yFound = false
-            for b in a
-                if b.include?(x) && b.include?(y)
-                    xEqY += 1
-                elsif b.include?(x)
-                    if !yFound
-                        xGtY += 1
-                    end
-                    xFound = true
-                elsif b.include?(y)
-                    if !xFound
-                        xLtY += 1
-                    end
-                    yFound = true
-                end
-            end
+    @input.each do |_, profile|
+      xFound = false
+      yFound = false
+      profile.each do |b|
+        if b.include?(x) && b.include?(y)
+          xEqY += 1
+        elsif b.include?(x)
+          if !yFound
+            xGtY += 1
+          end
+          xFound = true
+        elsif b.include?(y)
+          if !xFound
+            xLtY += 1
+          end
+          yFound = true
         end
-        [xGtY, xEqY, xLtY]
+      end
     end
+    print "Results: #{x} #{y} #{[xGtY, xEqY, xLtY]} \n"
+    [xGtY, xEqY, xLtY]
+  end
 
-#print compareTwo('a', 'c', votes)
+  def pairCombination(movies)
+    movies.combination(2).to_a
+  end
+
 end
--- spec/services/kemeny_service_spec.rb
+++ spec/services/kemeny_service_spec.rb
@@ -5,18 +5,36 @@ require_relative './../../app/services/parser_service.rb'
 
 RSpec.describe KemenyService do
   before :each do
-    @lexer = LexerService.new(File.readlines(File.expand_path('spec/files/test_strong_preferences.txt')))
-    @parser = ParserService.new()
-    #@kemeny = KemenyService.new([[['a'], ['b','c']], [['b'],['a'],['c']], [['b','a'],['c']], [['c'],['b'],['a']] ])
-    @kemeny = KemenyService.new(@lexer.preferences)
+    lexer = LexerService.new(File.readlines(File.expand_path('spec/files/test_strong_preferences.txt')))
+    @parser = ParserService.new(lexer.tokens)
+    @parser.parse_preference(lexer.preferences)
+    @movies = @parser.parse[:movies]
+    @kemeny = KemenyService.new(@parser.parse[:preferences])
   end
 
-  describe '#initialize' do
+  describe '#compareTwo' do
     it 'initializes properly' do
-      print @pr
-      expect(@kemeny.compareTwo(@lexer.movies[0], @lexer.movies[2])).to eq([0,3,0])
+      warcraft = @movies[0]
+      superman = @movies[2]
+      expect(@kemeny.compareTwo(warcraft, superman)).to eq([2,0,1])
     end
   end
+
+  describe '#pairCombinations' do
+    it 'gets correct pair combinations from parser input' do
+      expect(@kemeny.pairCombination(@movies)).to eq([["Warcraft", "Batman"], ["Warcraft", "Superman"], ["Batman", "Superman"]])
+    end
+  end
+
+  describe '#pairCombinations' do
+    it 'all pairs' do
+      @kemeny.pairCombination(@movies).each do |x, y|
+        @kemeny.compareTwo(x, y)
+      end
+    end
+  end
+
+
 
   """
   describe '#voters' do
