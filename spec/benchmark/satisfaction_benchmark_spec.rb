d53f5ebd8bbb9d8653e8ea01d0fc73805ed850aa
s103619 <s103619@student.dtu.dk>
2017-11-19 00:13:48 +0100

    updated satisfaction score

--- spec/benchmark/satisfaction_benchmark_spec.rb
+++ spec/benchmark/satisfaction_benchmark_spec.rb
@@ -1,174 +1,88 @@
 require 'spec_helper'
-require 'csv'
 require 'json'
 
-RSpec.describe 'Kemeny benchmarking' do
+RSpec.describe 'Schulze satisfaction benchmarking' do
   before :all do
-    @folder            = 'spec/benchmark_results'
+    @folder = 'spec/benchmark_results'
+    @result_folder = 'spec/satisfaction_results/schulze'
+    Dir.mkdir(@result_folder) unless File.exist?(@result_folder)
   end
 
-  describe '#Kemeny single round' do
+  describe '#Schulze single round' do
     it '5 voters' do
       puts 'Reading scenarios'
       scenarios = Dir['spec/benchmark_files/5_voters/*'].sort_by { |x| x.split('/').last.split('_').first.to_i }.map do |fname|
         JSON.parse(File.read(fname))
       end
 
-#      junk=CSV.open("#{@folder}/single_round_5_voters.csv",'r', col_step: '&')
-
-      # read the sql based .csv file, by lines
       junk = Array.new
       for a in IO.readlines("#{@folder}/schulze/single_round_5_voters.csv")
         ranking = a.split('&')[2].split(',')
         junk << ranking
       end
       junk.shift
-      puts junk.length
-      puts scenarios.length
-      satisfaction_score = 0
 
+      result = ['Alternatives&Score&Output']
+      satisfaction_score = 0
       junk.zip(scenarios).each do |results, scenarios|
+        average_score = 0.0
         scenarios['movie_preferences'].values.each do |prefs|
           winner = results[0]
           index = prefs.index(winner) + 1
           satisfaction_score += calculate_score(index.to_f+1, ranking.length.to_f)
+          average_score += calculate_score(index.to_f+1, ranking.length.to_f)
         end
+        result.push("#{scenarios['movies'].length}&#{average_score/scenarios['voters'].length.to_f}&#{scenarios['movie_preferences'].values.join(',')}")
       end
 
       puts 'final score: ', satisfaction_score /= (junk.length*5.to_f)
 
+      File.open("#{@result_folder}/single_round_5_voters.csv", 'w+') do |f|
+        f.puts(result)
+      end
+
     end
+
+
+    it '25 voters' do
+      puts 'Reading scenarios'
+      scenarios = Dir['spec/benchmark_files/25_voters/*'].sort_by { |x| x.split('/').last.split('_').first.to_i }.map do |fname|
+        JSON.parse(File.read(fname))
+      end
+
+      junk = Array.new
+      for a in IO.readlines("#{@folder}/schulze/single_round_25_voters.csv")
+        ranking = a.split('&')[2].split(',')
+        junk << ranking
+      end
+      junk.shift
+
+      result = ['Alternatives&Score&Output']
+      satisfaction_score = 0
+      junk.zip(scenarios).each do |results, scenarios|
+        average_score = 0.0
+        scenarios['movie_preferences'].values.each do |prefs|
+          winner = results[0]
+          index = prefs.index(winner) + 1
+          satisfaction_score += calculate_score(index.to_f+1, ranking.length.to_f)
+          average_score += calculate_score(index.to_f+1, ranking.length.to_f)
+        end
+        result.push("#{scenarios['movies'].length}&#{average_score/scenarios['voters'].length.to_f}&#{scenarios['movie_preferences'].values.join(',')}")
+      end
+
+      puts 'final score: ', satisfaction_score /= (junk.length*5.to_f)
+
+      File.open("#{@result_folder}/single_round_25_voters.csv", 'w+') do |f|
+        f.puts(result)
+      end
+
+    end
+
+
   end
 
   def calculate_score(index, length)
     (index/length-1)/(1.to_f/length-1)
   end
-
-
-      # CSV.open("#{@folder}/single_round_5_voters.csv", 'wb', col_sep: '&') do |csv|
-      #   csv << %w[Alternatives Time Output]
-      #
-      #   catch :too_slow do
-      #     scenarios.each_with_index do |scenario, idx|
-      #       puts "Benchmarking scenario #{idx}"
-      #
-      #       times   = []
-      #       @repetitions.times do |_|
-      #         start   = Time.now
-      #         @voting_service.kemeny(scenario)
-      #         finish  = Time.now
-      #         times << finish - start
-      #
-      #         if finish - start >= @timeout_threshold
-      #           puts 'Finished benchmark because it was too slow'
-      #           throw :too_slow
-      #         end
-      #       end
-      #
-      #       csv << [scenario['movies'].length, times.sum / times.length]
-      #     end
-      #   end
-      # end
-
-  #   it '25 voters' do
-  #     puts 'Reading scenarios'
-  #     scenarios = Dir['spec/benchmark_files/25_voters/*'].sort_by { |x| x.split('/').last.split('_').first.to_i }.map do |fname|
-  #       JSON.parse(File.read(fname))
-  #     end
-  #
-  #     CSV.open("#{@folder}/single_round_25_voters.csv", 'wb', col_sep: '&') do |csv|
-  #       csv << %w[Alternatives Time Output]
-  #
-  #       catch :too_slow do
-  #         scenarios.each_with_index do |scenario, idx|
-  #           puts "Benchmarking scenario #{idx}"
-  #
-  #           times   = []
-  #           winners = nil
-  #           @repetitions.times do |_|
-  #             start   = Time.now
-  #             winners = @kemeny.winner(scenario['movie_preferences'])
-  #             finish  = Time.now
-  #             times << finish - start
-  #
-  #             if finish - start >= @timeout_threshold
-  #               puts 'Finished benchmark because it was too slow'
-  #               throw :too_slow
-  #             end
-  #           end
-  #
-  #           csv << [scenario['movies'].length, times.sum / times.length, winners.join(',')]
-  #         end
-  #       end
-  #     end
-  #   end
-  # end
-  #
-  # describe '#Kemeny 3 rounds' do
-  #   it '5 voters' do
-  #     puts 'Reading scenarios'
-  #     scenarios = Dir['spec/benchmark_files/5_voters/*'].sort_by { |x| x.split('/').last.split('_').first.to_i }.map do |fname|
-  #       JSON.parse(File.read(fname))
-  #     end
-  #
-  #     CSV.open("#{@folder}/three_round_5_voters.csv", 'wb', col_sep: '&') do |csv|
-  #       csv << %w[Alternatives Time Output]
-  #
-  #       catch :too_slow do
-  #         scenarios.each_with_index do |scenario, idx|
-  #           puts "Benchmarking scenario #{idx}"
-  #
-  #           times   = []
-  #           @repetitions.times do |_|
-  #             start   = Time.now
-  #             @voting_service.kemeny(scenario)
-  #             finish  = Time.now
-  #             times << finish - start
-  #
-  #             if finish - start >= @timeout_threshold
-  #               puts 'Finished benchmark because it was too slow'
-  #               throw :too_slow
-  #             end
-  #           end
-  #
-  #           csv << [scenario['movies'].length, times.sum / times.length]
-  #         end
-  #       end
-  #     end
-  #   end
-  #
-  #   it '25 voters' do
-  #     puts 'Reading scenarios'
-  #     scenarios = Dir['spec/benchmark_files/25_voters/*'].sort_by { |x| x.split('/').last.split('_').first.to_i }.map do |fname|
-  #       JSON.parse(File.read(fname))
-  #     end
-  #
-  #     CSV.open("#{@folder}/three_round_25_voters.csv", 'wb', col_sep: '&') do |csv|
-  #       csv << %w[Alternatives Time Output]
-  #
-  #       catch :too_slow do
-  #         scenarios.each_with_index do |scenario, idx|
-  #           puts "Benchmarking scenario #{idx}"
-  #
-  #           times   = []
-  #           @repetitions.times do |_|
-  #             start   = Time.now
-  #             @voting_service.kemeny(scenario)
-  #             finish  = Time.now
-  #             times << finish - start
-  #
-  #             if finish - start >= @timeout_threshold
-  #               puts 'Finished benchmark because it was too slow'
-  #               throw :too_slow
-  #             end
-  #           end
-  #
-  #           csv << [scenario['movies'].length, times.sum / times.length]
-  #         end
-  #       end
-  #     end
-  #   end
-  # end
-
 
 end
